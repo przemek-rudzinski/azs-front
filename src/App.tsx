0bf6f2795dc214ce18b56efd9768e8e714a2fc6a
przemek-rudzinski <prudzinski36@wp.pl>
2022-08-02 01:58:21 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-green-500 text-center bg-green-200'>
+      <h1 className='text-3xl font-bold underline text-blue-500'>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
